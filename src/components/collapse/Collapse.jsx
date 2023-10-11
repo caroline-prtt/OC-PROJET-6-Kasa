29611a387eaf3a9bd0759c7e4d00c49a0955e713
Caroline Protat <caroline.protat@hotmail.fr>
2023-10-11 14:09:30 +0200

    ajout de l'animation sur l'icone fleche pour l'ouverture et la fermeture du collapse

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -6,23 +6,26 @@ function Collapse(props){
 
     // création et initalisation du State pour l'ouverture et fermeture collapse
     // [nomdustate, nomfonctionquimetàjour] = usestate(initialisationdustate)
-    const [collapseOpening, setIsOpen] = useState(false);
+    const [opening, setIsOpen] = useState(false);
 
     //Fonction change l'état du state de false à true / false etc..
-    function changeStateCollapse (){
-        setIsOpen(!collapseOpening);
+    function changeStateOpening (){
+        setIsOpen(!opening);
     };
 
-    console.log(collapseOpening);
+    console.log(opening);
+
+    //Condition pour nom de la classe de l'icone selon état ouverture
+    const buttonClassName = opening ? "collapse__header__button--open" : "collapse__header__button" ;
 
     return(
         <div className="collapse">
             <div className="collapse__header">
-                <h2 className="collapse_header__title">{props.title}</h2>
-                <img src={icone} onClick={changeStateCollapse} alt="icone flêche pour l'ouverture et la fermeture du collapse"/>
+                <h2 className="collapse__header__title">{props.title}</h2>
+                <img src={icone} onClick={changeStateOpening} className={`${buttonClassName} rotate`} alt="icone flêche pour l'ouverture et la fermeture du collapse"/>
             </div>
             {/* Ajout de la condition selon état affichache ou non de la div */}
-            {collapseOpening 
+            {opening 
                 ? (
                     <div className="collapse__content">
                         <p>{props.content}</p>
