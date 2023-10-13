bdd08489d7397863fb7b82726987ca20ce820fa3
Caroline Protat <caroline.protat@hotmail.fr>
2023-10-13 15:42:35 +0200

    création du composant Carrousel et intégration de ce dernier dans la page Accomodation

---
+++ src/components/carrousel/Carrousel.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import buttonPrev from "../../assets/button-previous.png"
+import buttonNext from "../../assets/button-next.png"
+import './carrousel.scss';
+
+function Carrousel(props) {
+
+    //Tableau de toutes les photos du logement sélectionné
+    const images = props.images;
+
+    //State de l'id de l'image visualisée dans le carroussel (NB : tableau donc premier id à zéro)
+    const [imageId, setimageId] = useState(0);
+
+    const nextImage = () => {
+        if (imageId === images.length - 1) {
+            // Si nous sommes déjà à la dernière image, revenir à la première
+            setimageId(0);
+        } else {
+            // Sinon, simplement passer à l'image suivante
+            setimageId(imageId + 1);
+        }
+    };
+
+    const prevImage = () => {
+        if (imageId === 0) {
+          // Si nous sommes déjà à la première image, revenir à la dernière
+          setimageId(images.length - 1);
+        } else {
+          // Sinon, simplement aller à l'image précédente
+          setimageId(imageId - 1);
+        }
+      };
+
+      //constante qui sera utilisée pour savoir s'il y a plusieurs photos
+      const hasMultipleImages = images.length > 1;
+
+  return (
+        <div className="carrousel">
+            <div className="carrousel__image">
+                <img src={images[imageId]} alt={`Photos de l'appartement ${props.title}`} />
+            </div>
+            {/* condition si plusieurs images alors les boutons et le conteur apparaissent */}
+            {hasMultipleImages ? (
+            <> 
+            <div className="carrousel__controls">
+                <img className="carrousel__controls__buttons" src={buttonPrev} onClick={prevImage} alt="bouton carrousel previous"></img>
+                <img className="carrousel__controls__buttons" src={buttonNext} onClick={nextImage} alt="bouton carrousel next" ></img>
+            </div>
+            <div className="carrousel__counter">
+                <span>{`${imageId + 1}/${images.length}`}</span>
+            </div>
+            </>
+            )
+            : null}
+        </div>
+    );
+}
+
+export default Carrousel;
--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import data from "../data.json"
+import PageNotFound from "./PageNotFound"
+import Carrousel from "../components/carrousel/Carrousel"
 
 function Accomodation (){
     // Dans le composant Gallery nous avions : 
@@ -12,19 +14,23 @@ function Accomodation (){
     const logement = data.find(item => item.id === id);
 
     if (!logement) {
-        // Si aucun logement avec cet ID n'est trouvé, vous pouvez gérer cette situation, par exemple, en affichant un message d'erreur.
-        return <div>Logement non trouvé</div>;
-
-        // Mettre le lien vers la page 404 !!!
+        // Si aucun logement avec cet ID n'est trouvé, affiche la page d'erreur 404
+        return <PageNotFound/>;
       }
 
     return (
-        <>
+        <main>
+            <div className="ficheLogement">
+                <section className="ficheLogement__carrousel">
+                    <Carrousel images={logement.pictures} title={logement.title}/>
+               </section>
+            </div>
+            
+            {/*  */}
             <p>Bienvenue sur la page présentant la fiche d'un logement ayant l'id suivant : {id}</p>
-            <img src={logement.cover} alt={logement.title}></img>
             <p>{logement.description}</p>
             <p>{logement.title}</p>
-        </>)
+        </main>)
 }
 
 export default Accomodation;
