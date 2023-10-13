217758ccced3e448547208299a26b88c6e849952
Caroline Protat <caroline.protat@hotmail.fr>
2023-10-13 17:41:14 +0200

    ajout d'une animation lors du défilement des photos du carrousel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -11,6 +11,35 @@ function Carrousel(props) {
     //State de l'id de l'image visualisée dans le carroussel (NB : tableau donc premier id à zéro)
     const [imageId, setimageId] = useState(0);
 
+    //fonction pour l'animation des images lors du changement de photo
+    const animationImageTransition = () => {
+        
+        const imageElement = document.querySelector(".carrousel__image img");
+    
+        if (imageElement) {
+            // Définit une transition pour les propriétés "transform" et "opacity"
+            imageElement.style.transition = "transform 300ms, opacity 300ms";
+            // Réduit progressivement l'opacité de l'image pour créer un effet de fondu en laissant 15 % d'opacité.
+            imageElement.style.opacity = 0.85;
+            // Réduit la taille de l'image à 97 % de sa taille d'origine pour créer un effet de réduction vers le centre.
+            imageElement.style.transform = "scale(0.97)";
+
+            // Après un court délai (200 millisecondes), réinitialisez les propriétés.
+            setTimeout(() => {
+                // Désactive temporairement la transition pour réinitialiser les propriétés sans animation.
+                imageElement.style.transition = "none";
+                // Rétablit complètement l'opacité de l'image (1) et réinitialise la position (aucun déplacement ni réduction).
+                imageElement.style.opacity = 1;
+                imageElement.style.transform = "none";
+                // Réactive la transition pour les futurs changements d'image.
+                // (Désactive temporairement puis réactive pour éviter que l'animation ne se déclenche immédiatement)
+                setTimeout(() => {
+                    imageElement.style.transition = "transform 300ms, opacity 300ms";
+                }, 0);
+            }, 200); // Ce délai détermine la durée pendant laquelle l'animation du fondu vers le centre se produit.
+        }
+    }
+
     const nextImage = () => {
         if (imageId === images.length - 1) {
             // Si nous sommes déjà à la dernière image, revenir à la première
@@ -19,6 +48,8 @@ function Carrousel(props) {
             // Sinon, simplement passer à l'image suivante
             setimageId(imageId + 1);
         }
+
+        animationImageTransition()
     };
 
     const prevImage = () => {
@@ -29,6 +60,7 @@ function Carrousel(props) {
           // Sinon, simplement aller à l'image précédente
           setimageId(imageId - 1);
         }
+        animationImageTransition()
       };
 
       //constante qui sera utilisée pour savoir s'il y a plusieurs photos
@@ -42,11 +74,11 @@ function Carrousel(props) {
             {/* condition si plusieurs images alors les boutons et le conteur apparaissent */}
             {hasMultipleImages ? (
             <> 
-            <div className="carrousel__controls">
+            <div className="carrousel__controls buttons">
                 <img className="carrousel__controls__buttons" src={buttonPrev} onClick={prevImage} alt="bouton carrousel previous"></img>
                 <img className="carrousel__controls__buttons" src={buttonNext} onClick={nextImage} alt="bouton carrousel next" ></img>
             </div>
-            <div className="carrousel__counter">
+            <div className="carrousel__controls counter">
                 <span>{`${imageId + 1}/${images.length}`}</span>
             </div>
             </>
