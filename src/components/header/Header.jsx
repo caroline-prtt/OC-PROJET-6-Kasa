4418ee64d6b5235d89f3dd6d5313571e8902a045
Caroline Protat <caroline.protat@hotmail.fr>
2023-10-23 12:18:28 +0200

    modification du composant Header : remplacement des composants Link en NavLink pour intégrer le style souligné des liens actifs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logoHeader from "../../assets/logo-header.png"
 import Logo from "../logo/Logo";
 import "../header/header.scss"
@@ -9,8 +9,8 @@ function Header (){
             <nav className="navbar">
                 <Logo src={logoHeader} alt="logo kasa rouge" className="navbar__logo"/>
                 <div>
-                    <Link to="/" className="navbar__link">Accueil</Link>
-                    <Link to="/about" className="navbar__link">A Propos</Link>
+                    <NavLink exact="true" to="/" className="navbar__link" activeclassname="active">Accueil</NavLink>
+                    <NavLink to="/about" className="navbar__link" activeclassname="active">A Propos</NavLink>
                 </div>
             </nav>
         </header>
