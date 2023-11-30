4c41c9f480d21865d922ba8c5d1b09982dd06fdd
Caroline Protat <caroline.protat@hotmail.fr>
2023-11-30 16:26:39 +0100

    essai correction route pour déploiement github pages

--- src/index.jsx
+++ src/index.jsx
@@ -9,8 +9,7 @@ import "./index.scss"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Ajout basename pour adapter l'URL suite au déploiement sur github pages (nom du repo)*/}
-    <Router basename="/OC-PROJET-6-Kasa"> 
+    <Router> 
       <Header/>
       <RoutesApp/>
       <Footer/>
